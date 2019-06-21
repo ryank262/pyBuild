bf7088be232d02401bdeb95c14d898fa73b4ae69
Ryan Kirkpatrick <ryan.kirkpatrick2@resmed.com>
2019-06-21 10:15:57 -0700

    latest

---
+++ build/lib/shellutil/__init__.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+import shutil
+import subprocess
+import sys
+import re
+import time
+import glob
+
+def __shell(cmd, env_vars=None, should_return_output=False, continue_if_fail=False, suppress_output=False):
+    env = os.environ.copy()
+
+    # needs to be surrounded with single quotes,
+    # otherwise inner double quotes need to be escaped
+    formatted_cmd = f"/bin/bash -c '{cmd}'"
+    if env_vars is not None:
+        for k, v in env_vars.items():
+            env[k] = v
+    try:
+        print(f'`{cmd}`')
+
+        if should_return_output:
+            output = subprocess.run(formatted_cmd, env=env, shell=True, check=True, text=True, stdout=subprocess.PIPE).stdout
+            if not suppress_output:
+                print(output)
+            return output.strip()
+        else:
+            std_out = subprocess.DEVNULL if suppress_output else None
+            subprocess.run(formatted_cmd, env=env, shell=True, check=True, text=True, stdout=std_out)
+
+    except subprocess.CalledProcessError as error:
+        if not continue_if_fail:
+            raise error
+
+
+def shell_value(cmd, continue_if_fail=False, suppress_output=False):
+    """
+    Runs a command in shell -- returns and prints the output at the end.
+
+    :param suppress_output: if the python script should suppress the output from the shell command
+    :param continue_if_fail: if the python script should continue after an failure or if it should raise an exception
+    :param cmd: string version of the command to be run
+    :return: output of the cmd
+    """
+
+    return __shell(cmd, should_return_output=True, continue_if_fail=continue_if_fail, suppress_output=suppress_output)
+
+
+def shell(cmd, continue_if_fail=False, suppress_output=False):
+    """
+    Runs a command as if it were in run in shell.
+
+    :param suppress_output: if the python script should suppress the output from the shell command
+    :param continue_if_fail: if the python script should continue after an failure or if it should raise an exception
+    :param cmd: string of the cmd to be run
+    :return: nothing
+    """
+
+    __shell(cmd, continue_if_fail=continue_if_fail, suppress_output=suppress_output)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shellutil',
+      version='0.1',
+      description='A shell',
+      url='https://github.com/ryank262/pyBuild',
+      author='Ryan Kirkpatrick',
+      author_email='ryan.Kirkpatrick2@resmed.com',
+      license='MIT',
+      packages=['shellutil'],
+      zip_safe=False)
