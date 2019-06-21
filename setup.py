399ebe768e30d8cc9c35ea9ac237af28c37e932c
Ryan Kirkpatrick <ryan.kirkpatrick2@resmed.com>
2019-06-21 15:49:35 -0700

    Add package dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='shellutil',
-      version='0.2',
+      version='0.3',
       description='A shell',
       url='https://github.com/ryank262/pyBuild',
       author='Ryan Kirkpatrick',
       author_email='ryan.Kirkpatrick2@resmed.com',
       license='MIT',
       packages=['shellutil'],
+      install_requires=['future', 'builtins', 'past', 'six'],
       zip_safe=False)
